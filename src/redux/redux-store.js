4262f9408d0f6edd3d9e689382d4c35cc6d56093
Nikolay <30789678+Nikolaiy@users.noreply.github.com>
2020-04-23 23:15:03 +0500

    added login form

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -4,14 +4,16 @@ import messageReducer from "./message-reducer";
 import usersReducer from "./users-reducer";
 import friendReducer from "./friend-reducer";
 import authReducer from "./auth-reducer";
-import thunkMiddleware from 'redux-thunk'
+import thunkMiddleware from 'redux-thunk';
+import { reducer as formReducer } from 'redux-form';
 
 let redusers = combineReducers({
     profilePage: profileReducer,
     messagePage: messageReducer,
     usersPage: usersReducer,
     friendPage: friendReducer,
-    authReducer : authReducer
+    authReducer : authReducer,
+    form: formReducer,
 });
 
 let store = createStore(redusers, applyMiddleware(thunkMiddleware));
