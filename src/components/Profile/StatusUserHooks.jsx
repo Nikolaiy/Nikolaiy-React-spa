4c4017bb5105bccfc0bb6f4f7f2c71f662c710fc
Nikolay <30789678+Nikolaiy@users.noreply.github.com>
2020-04-28 01:59:02 +0500

    added useEffect

--- src/components/Profile/StatusUserHooks.jsx
+++ src/components/Profile/StatusUserHooks.jsx
@@ -1,23 +1,26 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 const StatusUserHooks = (props) => {
 
     let [editMode, setEditMode] = useState(false);
     let [status, setStatus] = useState(props.status);
 
+    useEffect( () => {
+        setStatus(props.status)
+    }, [props.status]);
 
     let activeidEditMode = () => {
         setEditMode(true)
-    }
+    };
 
     let deactivateEditMode = () => {
         setEditMode(false)
         props.updateStatus(status)
-    }
+    };
 
     let onStatusChange = (e) => {
         setStatus(e.currentTarget.value)
-    }
+    };
 
     return (
         <div>
