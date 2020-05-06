5bbcb67d3d3e708ffe6f87d5e789735e87f61718
Nikolay <30789678+Nikolaiy@users.noreply.github.com>
2020-05-06 23:41:36 +0500

    added css bootstrap and modified FormsControl

--- src/common/FormsControls/FormsControls.js
+++ src/common/FormsControls/FormsControls.js
@@ -4,9 +4,11 @@ import s from "./FormsControls.module.css"
 export const Textarea = ({input, meta: {touched, error}, ...props}) => {
     let hasError = error && touched;
     return (
-        <div>
-            <textarea {...input} {...props} className={(hasError ? s.inputError : '')} />
-            {hasError && <div className={s.textError}>{error}</div>}
+        <div className="form-group">
+            <form action="" className=''>
+                <textarea {...input} {...props} className={(hasError ? s.inputError : '')}/>
+                {hasError && <div className={s.textError}>{error}</div>}
+            </form>
         </div>
     )
 }
@@ -14,8 +16,8 @@ export const Textarea = ({input, meta: {touched, error}, ...props}) => {
 export const Input = ({input, meta: {touched, error}, ...props}) => {
     let hasError = error && touched;
     return (
-        <div>
-            <input {...input} {...props} className={(hasError ? s.inputError : '')} />
+        <div className="form-group">
+            <input {...input} {...props} className={(hasError ? s.inputError : '')}/>
             {hasError && <div className={s.textError}>{error}</div>}
         </div>
     )
